61bd744e103c9f745a2014a5fff15e4d479d550d
Franklin van de Meent <fr@nkl.in>
2016-04-19 04:32:06 +0200

    Clean up

--- readcsv.js
+++ readcsv.js
@@ -8,65 +8,64 @@ License:       Unlicense / Public Domain
                (https://github.com/fvdm/nodejs-readcsv/raw/master/LICENSE)
 */
 
-var fs = require('fs')
+var fs = require ('fs');
 
-module.exports = function( head, file, callback ) {
-	if( typeof file === 'function' ) {
-		var callback = file
-		var file = head
-		var head = null
-	}
+module.exports = function (head, file, callback) {
+  if (typeof file === 'function') {
+    callback = file;
+    file = head;
+    head = null;
+  }
 
-	fs.readFile( file, function( err, data ) {
-		if( err ) {
-			callback( err )
-			return
-		}
-		
-		var output = []
-		data = data.toString('utf8')
-		
-		var linebreak = data.slice(-2) === '\r\n' ? '\r\n' : '\n'
-		data = data.trim().split( linebreak )
-		
-		var sep = ','
-		var quotes = '\''
-		
-		if( data[0].match('\',\'') ) {
-			sep = ','
-			quotes = '\''
-		} else if( data[0].match('\';\'') ) {
-			sep = ';'
-			quotes = '\''
-		} else if( data[0].match('","') ) {
-			sep = ','
-			quotes = '"'
-		} else if( data[0].match('";"') ) {
-			sep = ';'
-			quotes = '"'
-		} else {
-			callback( new Error('cannot detect line format') )
-		}
-		
-		data.forEach( function( line ) {
-			line = line.split( quotes + sep + quotes )
-			line[0] = line[0].slice(1)
-			line[ line.length -1 ] = line[ line.length -1 ].slice(0,-1)
-			if( head ) {
-				var tx = {}
-				head.forEach( function( name, key ) {
-					tx[ name ] = line[ key ]
-				})
-				output.push( tx )
-				delete tx
-			} else {
-				output.push( line )
-			}
-			delete line
-		})
-		
-		delete data
-		callback( null, output )
-		delete output
-	})
-}
+  fs.readFile (file, function (err, data) {
+    var output = [];
+    var sep = ',';
+    var quotes = '\'';
+    var linebreak;
+
+    if (err) {
+      callback (err);
+      return;
+    }
+    
+    data = data.toString ('utf8');
+    linebreak = data.slice (-2) === '\r\n' ? '\r\n' : '\n';
+    data = data.trim () .split (linebreak);
+    
+    if (data [0] .match ('\',\'')) {
+      sep = ',';
+      quotes = '\'';
+    } else if (data [0] .match ('\';\'')) {
+      sep = ';';
+      quotes = '\'';
+    } else if (data [0] .match ('","')) {
+      sep = ',';
+      quotes = '"';
+    } else if (data [0] .match ('";"')) {
+      sep = ';';
+      quotes = '"';
+    } else {
+      callback (new Error ('cannot detect line format'));
+      return;
+    }
+    
+    data.forEach (function (line) {
+      var tx = {};
+
+      line = line.split (quotes + sep + quotes);
+      line [0] = line [0] .slice (1);
+      line [line.length - 1] = line [line.length - 1] .slice (0, -1);
+
+      if (head) {
+        head.forEach (function (name, key) {
+          tx [name] = line [key];
+        });
+        output.push (tx);
+      } else {
+        output.push (line);
+      }
+    });
+    
+    callback (null, output);
+  });
+};
