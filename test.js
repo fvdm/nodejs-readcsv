e838ef79f60915c7b9e705d04494aa486821e4d3
Franklin van de Meent <fr@nkl.in>
2017-12-06 01:45:09 +0100

    Test(main): Implement test csv files

--- test.js
+++ test.js
@@ -1,12 +1,31 @@
 var dotest = require ('dotest');
 var app = require ('./');
 
+function testFile (file, test) {
+  app (file, (err, data) => {
+    test (err)
+      .isArray ('fail', 'data', data)
+      .isNotEmpty ('fail', 'data', data)
+      .isObject ('fail', 'data[0]', data && data[0])
+      .isExactly ('fail', 'data[0].One', data && data[0] && data[0].One, 'Hello')
+      .isExactly ('fail', 'data[0].One', data && data[0] && data[0].Two, 'World')
+      .isExactly ('fail', 'data[0].One', data && data[0] && data[0].Three, '!!')
+      .done();
+  });
+}
 
-dotest.add ('Module', function (test) {
-  test ()
+dotest.add ('Module', test => {
+  test()
     .isFunction ('fail', 'exports', app)
     .done ();
 });
 
+dotest.add ('Comma-quoted', test => {
+  testFile ('./test/test.comma-quoted.csv', test);
+});
+
+dotest.add ('Semi-quoted', test => {
+  testFile ('./test/test.semi-quoted.csv', test);
+});
 
 dotest.run ();
